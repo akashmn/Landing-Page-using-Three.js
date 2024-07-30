4c83919906d18bbf1a6516ded577b8a98290e447
Akash M Nandan <akash003nandan@gmail.com>
2024-07-30 19:04:47 +0530

    demo for three.js running in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ThreeScene from "./assets/ThreeScene"
 
 const App = () => {
   return (
-    <div className='text-4xl'>App</div>
+    <div className='text-4xl bg-black flex flex-col items-center'>
+      <h2 className="text-white">vite.js + tailwindCSS + Three.js</h2>
+      <ThreeScene/>
+    </div>
   )
 }
 
