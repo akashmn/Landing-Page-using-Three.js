da8f1fdae4e118dbd62052a45c54fd2172b6c17b
Akash M Nandan <akash003nandan@gmail.com>
2024-07-31 19:41:38 +0530

    loader with reveal and pulse set

--- src/assets/Components/Loader.jsx
+++ src/assets/Components/Loader.jsx
@@ -1,10 +1,10 @@
-
+import '../Styles/Loader.css'
 
 const Loader = () => {
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center gap-3">
-        <p className="h-[100px] w-[80px] text-8xl font-extrabold text-orange-500 z-0">Q</p>
-        <p className="h-[20px] w-[150px] bg-white text-black text-xsl font-light z-20 mb-16">Qmark Technolabs</p>
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-3 animate-pulse">
+        <p className="h-[100px] w-[80px] text-8xl font-extrabold text-orange-500 z-0 animate-reveal">Q</p>
+        <p className="h-[100px] w-[150px] bg-white text-black text-xsl font-light z-20 mb-16">Qmark Technolabs</p>
     </div>
   )
 }
