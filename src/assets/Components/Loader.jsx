e221eacedffe726ba00201887b39f0c24d8bce8c
Akash M Nandan <akash003nandan@gmail.com>
2024-07-31 19:16:54 +0530

    loader styling set

--- src/assets/Components/Loader.jsx
+++ src/assets/Components/Loader.jsx
@@ -2,11 +2,9 @@
 
 const Loader = () => {
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center overflow-hidden">
-      <div className="flex flex-col items-center justify-center gap-3 mb-10">
-        <p className="text-8xl font-extrabold text-orange-500">Q</p>
-        <p className="text-black text-xsl font-light">Qmark Technolabs</p>
-      </div>
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-3">
+        <p className="h-[100px] w-[80px] text-8xl font-extrabold text-orange-500 z-0">Q</p>
+        <p className="h-[20px] w-[150px] bg-white text-black text-xsl font-light z-20 mb-16">Qmark Technolabs</p>
     </div>
   )
 }
