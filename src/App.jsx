74106344a1b73f78f168396953af3b9a98993389
Akash M Nandan <akash003nandan@gmail.com>
2024-07-31 13:18:49 +0530

    loader structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Loader from "./assets/Components/Loader"
 
 
 const App = () => {
   return (
-    <div className='text-4xl bg-black flex flex-col items-center'>
-      <h2 className="text-white">vite.js + tailwindCSS + Three.js</h2>
-    </div>
+    <Loader />
   )
 }
 
---
+++ src/assets/Components/Loader.jsx
@@ -0,0 +1,15 @@
+
+
+const Loader = () => {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-3 mb-10">
+        <p className="text-8xl font-extrabold text-orange-500">Q</p>
+        <p className="text-black text-xsl font-light">Qmark Technolabs</p>
+      </div>
+      <div className="bg-slate-500"></div>
+    </div>
+  )
+}
+
+export default Loader
