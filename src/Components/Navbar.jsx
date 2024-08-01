fda10e0db0dce7ca7c8d0dd829c19c9fe5196261
Akash M Nandan <akash003nandan@gmail.com>
2024-08-01 20:37:30 +0530

    added active class on navbar links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,9 +4,14 @@ import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const [popUp, setPopUp] = useState(false);
+  const [activeLink, setActiveLink] = useState('');
 
   const togglePopup = () => {
     setPopUp(!popUp);
+  };
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
   };
 
   return (
@@ -14,9 +19,27 @@ const Navbar = () => {
       <div className="h-[75px] w-full rounded-full border-2 border-black flex flex-row items-center justify-between px-16">
         <p>Qmark Technolabs</p>
         <div className="flex flex-row gap-10">
-          <a href="">Home</a>
-          <a href="">Works</a>
-          <a href="">Team</a>
+          <a
+              href="#"
+              className={`${activeLink === 'Home' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Home')}
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className={`${activeLink === 'Works' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Works')}
+            >
+              Works
+            </a>
+            <a
+              href="#"
+              className={`${activeLink === 'Team' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Team')}
+            >
+              Team
+            </a>
           <button onClick={togglePopup}>
             <FontAwesomeIcon
               icon={popUp ? faCaretUp : faCaretDown}
@@ -33,9 +56,27 @@ const Navbar = () => {
       </div>
       {popUp && (
         <div className='ml-[750px] -mt-4 p-6 z-20 bg-black h-[200px] w-[250px] absolute rounded-xl transition duration-700 ease-in-out opacity-95 flex flex-col gap-2'>
-          <a href="" className='text-white p-3 hover:underline'>Careers</a>
-          <a href="" className='text-white p-3 hover:underline'>Services</a>
-          <a href="" className='text-white p-3 hover:underline'>More</a>
+          <a
+            href="#"
+            className={`text-white p-3 hover:underline ${activeLink === 'Careers' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('Careers')}
+          >
+            Careers
+          </a>
+          <a
+            href="#"
+            className={`text-white p-3 hover:underline ${activeLink === 'Services' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('Services')}
+          >
+            Services
+          </a>
+          <a
+            href="#"
+            className={`text-white p-3 hover:underline ${activeLink === 'More' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('More')}
+          >
+            More
+          </a>
         </div>
       )}
     </div>
