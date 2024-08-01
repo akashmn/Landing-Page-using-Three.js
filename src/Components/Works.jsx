34dcceeb3fb22fbf4774dbbd18f551d7bc3e0ca4
Akash M Nandan <akash003nandan@gmail.com>
2024-08-01 22:38:50 +0530

    container set created

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,21 +1,21 @@
-
-
 const Works = () => {
-  return (
-    <div className="z-0 py-4">
-        <div className="bg-yellow-200 w-full h-screen py-20">
-            <center className="">
-                <p className="text-6xl text-black">Our Works</p>
-                <p className="text-2xl opacity-50">So Far, So Good.</p>
-            </center>
-            <div className="flex flex-row items-center justify-center gap-1">
-                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
-                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
-                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
-            </div>
+    return (
+      <div className="z-0 py-4">
+        <div className="bg-yellow-200 w-full h-screen py-10">
+          <center className="">
+            <p className="text-6xl text-black">Our Works</p>
+          </center>
+          <div className="flex flex-wrap items-center mt-10 justify-center px-[200px]">
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+          </div>
         </div>
-    </div>
-  )
-}
-
-export default Works
+      </div>
+    );
+  };
+  
+  export default Works;
