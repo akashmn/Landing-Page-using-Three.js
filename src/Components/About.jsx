0182a66f0a9d0a208aebc8ed5300dfa3e59529ec
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 02:19:20 +0530

    position adjusted

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@
 const About = () => {
   return (
     <div className="py-3">
-        <div className="h-screen w-full bg-green-600 px-36">
+        <div className="h-screen w-full bg-green-600 px-36 py-16">
             <div className="rotate-[-12deg] h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300">
                 <div className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                     <p className="text-2xl tracking-wider px-5 py-5 uppercase">We design</p>
