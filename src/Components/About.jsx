6c0babb202007f2d87896159fe53906a8a3686d7
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 03:00:59 +0530

    about us section set

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -43,8 +43,8 @@ const About = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-10 ml-52">
                 <p className="text-5xl tracking-wide">Presenting Ourselves!</p>
-                <p>Our team of passionate designers, developers, and marketing specialists collaborates to bring your vision to life. We create beautiful, user-friendly websites and mobile apps, craft compelling design elements that elevate your brand, and implement data-driven digital marketing strategies to reach your target audience.</p>
-                <p>At Qmark Technolabs, we offer a spectrum of services designed to meet the diverse needs of businesses today.</p>
+                <p className="text-lg">Our team of passionate designers, developers, and marketing specialists collaborates to bring your vision to life. We create beautiful, user-friendly websites and mobile apps, craft compelling design elements that elevate your brand, and implement data-driven digital marketing strategies to reach your target audience.</p>
+                <p className="text-lg">At Qmark Technolabs, we offer a spectrum of services designed to meet the diverse needs of businesses today.</p>
             </div>
         </div>
     </div>
