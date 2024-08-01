36ed4e50dbf0af8957a80a5166b09f75939a6be6
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 00:12:13 +0530

    image added to all the container

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -11,16 +11,81 @@ const Works = () => {
           <center className="">
             <p className="text-6xl text-black">Our Works</p>
           </center>
-          <div className="flex flex-wrap items-center mt-10 justify-center px-[200px]">
+          <div className="flex flex-wrap items-center mt-8 justify-center px-[200px] gap-1">
             
-            <div className="h-[280px] w-[320px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
                 <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
                     <img src={work1} alt="" className='w-full h-full' />
                 </div>
                 <div className='flex flex-row items-center justify-between px-2 pr-4'>
-                    <div className='mt-3 flex flex-col items-start px-2'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
                         <p className='text-sm opacity-70'>Web Development</p>
-                        <p className='mt-7 text-lg'>SP-MEDIPORT</p>
+                        <p className='mt-4 text-lg'>SP-MEDIPORT</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
+                </div>
+            </div>
+
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work2} alt="" className='w-full h-full' />
+                </div>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>Web Development</p>
+                        <p className='mt-4 text-lg'>KMA</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
+                </div>
+            </div>
+
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work3} alt="" className='w-full h-full' />
+                </div>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>Web Development</p>
+                        <p className='mt-4 text-lg'>WSO</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
+                </div>
+            </div>
+
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work4} alt="" className='w-full h-full' />
+                </div>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>App Development</p>
+                        <p className='mt-4 text-lg'>THANAL</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
+                </div>
+            </div>
+
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work5} alt="" className='w-full h-full' />
+                </div>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>Web Development</p>
+                        <p className='mt-4 text-lg'>AISEF</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
+                </div>
+            </div>
+
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work4} alt="" className='w-full h-full' />
+                </div>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-2 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>App Development</p>
+                        <p className='mt-4 text-lg'>THANAL</p>
                     </div>
                     <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
                 </div>
