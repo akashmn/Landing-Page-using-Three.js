45fd7dd769b27018068b5a8d537a821a24e3c03f
Akash M Nandan <akash003nandan@gmail.com>
2024-08-01 21:32:15 +0530

    single project container created

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -8,6 +8,11 @@ const Works = () => {
                 <p className="text-6xl text-black">Our Works</p>
                 <p className="text-2xl opacity-50">So Far, So Good.</p>
             </center>
+            <div className="flex flex-row items-center justify-center gap-1">
+                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
+                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
+                <div className="h-[300px] w-[350px] bg-white rounded-3xl border-[3px] border-black"></div>
+            </div>
         </div>
     </div>
   )
