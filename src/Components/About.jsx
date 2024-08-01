829158167ad6c6e6e7973ceb2be95171a93311ea
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 02:17:58 +0530

    rotation of 3 containers set

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -27,6 +27,18 @@ const About = () => {
                     <p className="text-2xl tracking-wider px-1 py-5 uppercase">We promote</p>
                 </div>
             </div>
+
+            <div className="rotate-[-20deg] ml-10 -mt-14 h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300">
+                <div className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <p className="text-2xl tracking-wider px-5 py-5 uppercase">We design</p>
+                </div>
+                <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <p className="text-2xl tracking-wider px-2 py-5 uppercase">We develop</p>
+                </div>
+                <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <p className="text-2xl tracking-wider px-1 py-5 uppercase">We promote</p>
+                </div>
+            </div>
         </div>
     </div>
   )
