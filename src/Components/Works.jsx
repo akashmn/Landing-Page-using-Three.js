c8bcde8f3b72b16af28f2a955fff7be427762a8c
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 00:00:29 +0530

    single container set

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -13,13 +13,16 @@ const Works = () => {
           </center>
           <div className="flex flex-wrap items-center mt-10 justify-center px-[200px]">
             
-            <div className="h-[280px] w-[320px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500">
+            <div className="h-[280px] w-[320px] bg-white rounded-3xl border-[3px] border-black p-2 hover:bg-sky-500 transition-all duration-300">
                 <div className="h-[170px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
                     <img src={work1} alt="" className='w-full h-full' />
                 </div>
-                <div className='mt-2 flex flex-col items-start px-2'>
-                    <p className='text-sm opacity-70'>Web Development</p>
-                    <p className='mt-5'>SP-MEDIPORT</p>
+                <div className='flex flex-row items-center justify-between px-2 pr-4'>
+                    <div className='mt-3 flex flex-col items-start px-2'>
+                        <p className='text-sm opacity-70'>Web Development</p>
+                        <p className='mt-7 text-lg'>SP-MEDIPORT</p>
+                    </div>
+                    <button className='bg-slate-200 hover:bg-black border-2 border-black rounded-2xl h-[50px] w-[60px]'>👀</button>
                 </div>
             </div>
           </div>
