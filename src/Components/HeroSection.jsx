672aef55f24b51e2378b670084ed5239206af580
Akash M Nandan <akash003nandan@gmail.com>
2024-08-01 19:30:34 +0530

    container for google badge

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,7 +3,15 @@
 const HeroSection = () => {
   return (
     <div className="z-0 px-6 py-4">
-        <div className="h-[620px] w-full rounded-3xl border-black border-2">
+        <div className="h-[620px] w-full rounded-3xl border-black border-2 flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-7xl">The Loud Soluion For</p>
+            <p className="text-8xl">Unheard Problems.</p>
+          </div>
+
+          <div>
+            <div className="h-[150px] w-[300px] border-2 border-black rounded-3xl"></div>
+          </div>
         </div>
     </div>
   )
