c62dcae93772fb3b804e1f0b27d0aae9de440e2c
Akash M Nandan <akash003nandan@gmail.com>
2024-08-01 23:13:42 +0530

    image added in one container

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,12 +1,18 @@
+import work1 from '../assets/Images/work1.png'
+
 const Works = () => {
     return (
-      <div className="z-0 py-4">
-        <div className="bg-yellow-200 w-full h-screen py-10">
+      <div className="py-4">
+        <div className="bg-yellow-200 h-screen w-full py-10 z-20">
           <center className="">
             <p className="text-6xl text-black">Our Works</p>
           </center>
           <div className="flex flex-wrap items-center mt-10 justify-center px-[200px]">
-            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
+            <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black p-3 hover:bg-sky-500">
+                <div className="h-[150px] bg-white rounded-3xl border-black border-2 hover overflow-hidden">
+                    <img src={work1} alt="" className='w-full h-full' />
+                </div>
+            </div>
             <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
             <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
             <div className="h-[260px] w-[300px] bg-white rounded-3xl border-[3px] border-black"></div>
