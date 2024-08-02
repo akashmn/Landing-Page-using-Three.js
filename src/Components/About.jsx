2d6a1fa461886c6654930ea1672872b88d7b1d72
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 21:28:44 +0530

    AOS added to the figure in AboutSec

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,38 +5,62 @@ const About = () => {
     <div className="py-3 flex flex-row">
         <div className="h-screen w-full bg-green-600 px-[250px] py-16 flex flex-row items-center justify-between">
             <div>
-                <div className="rotate-[-12deg] h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300">
-                    <div className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                <div data-aos="fade-right" 
+                    data-aos-duration="500"
+                    className="rotate-[-12deg] h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300 -z-20">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="2000"
+                         className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-5 py-5 uppercase">We design</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="2500"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-2 py-5 uppercase">We develop</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="3000"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-1 py-5 uppercase">We promote</p>
                     </div>
                 </div>
 
-                <div className="rotate-[12deg] ml-28 -mt-36 h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300">
-                    <div className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                <div data-aos="fade-right"
+                    data-aos-duration="900"
+                    className="rotate-[12deg] ml-28 -mt-36 h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300 z-200">
+                    <div  data-aos="zoom-in" 
+                         data-aos-duration="2000"
+                         className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-2 py-5 uppercase">We Develop</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="2500"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-2 py-5 uppercase">We support</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="3000"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-[1.45rem] tracking-wider px-1 py-5 uppercase">We Empower</p>
                     </div>
                 </div>
 
-                <div className="rotate-[-20deg] ml-10 -mt-14 h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300">
-                    <div className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                <div data-aos="fade-right"
+                    data-aos-duration="1300"
+                    className="rotate-[-20deg] ml-10 -mt-14 h-[260px] w-[230px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 hover:bg-sky-500 transition-all duration-300 z-100">
+                    <div  data-aos="zoom-in" 
+                         data-aos-duration="2000"
+                         className="h-[70px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-1 py-5 uppercase">We promote</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="2500"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-2xl tracking-wider px-2 py-5 uppercase">We provide</p>
                     </div>
-                    <div className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
+                    <div data-aos="zoom-in" 
+                         data-aos-duration="3000"
+                         className="h-[70px] w-full bg-white border-black border-2 rounded-2xl flem items-center justify-center">
                         <p className="text-[1.41rem] tracking-wider px-1 py-5 uppercase">We Integrate</p>
                     </div>
                 </div>
