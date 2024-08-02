3db99562bc10c920c3a7f90426342e40fe816322
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 19:34:31 +0530

    zoom in effect added on tabs in heroSection

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -17,18 +17,24 @@ const HeroSection = () => {
                   The Loud Soluion For</p>
             <p className="text-8xl" 
                 data-aos="fade-up"
-                data-aos-duration="1600">Unheard Problems.</p>
+                data-aos-duration="1600"
+                data-aos-anchor-placement="bottom-bottom"
+                  >Unheard Problems.</p>
           </div>
 
           <div className='flex flex-row gap-5'>
-            <div className='h-[115px] w-[300px] flex flex-col items-center justify-end gap-4'>
-              <p className='text-base'>A Tech-Thinking Company <br /><center>That Drives <b>50+</b> Brands</center></p>
+            <div data-aos="zoom-in"
+                data-aos-duration="1200" 
+                className='h-[115px] w-[300px] flex flex-col items-center justify-end gap-4'>
+                  <p className='text-base'>A Tech-Thinking Company <br /><center>That Drives <b>50+</b> Brands</center></p>
               <button className='transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black'>
                 Join us
               </button>
             </div>
-            <div className="h-[115px] w-[300px] border-2 border-black rounded-3xl overflow-hidden">
-              <img src={googleBadge} alt="" className="h-full w-full object-fill"/>
+            <div data-aos="zoom-in"
+                 data-aos-duration="1200"
+                 className="h-[115px] w-[300px] border-2 border-black rounded-3xl overflow-hidden">
+                  <img src={googleBadge} alt="" className="h-full w-full object-fill"/>
             </div>
           </div>
         </div>
