51667d13b669e3e408da97f42617517dba2c38c8
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 14:50:54 +0530

    send button set

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,13 +3,16 @@
 const Footer = () => {
   return (
     <div className="h-[500px] w-full px-24 pt-16">
-      <div className="h-[400px] w-full bg-black rounded-3xl px-20 py-16">
+      <div className="h-[400px] w-full bg-black rounded-3xl px-20 py-14">
         <div className="">
           <div className="flex flex-col">
             <p className="h-[80px] w-[250px] border-white border-2 bg-green-700 text-4xl text-white rounded-full px-8 py-5">Subscribe</p>
             <p className="h-[80px] w-[345px] border-white border-2 text-4xl text-white rounded-full px-9 py-5">to our updates</p>
           </div>
-          
+          <div>
+            <input className="h-[65px] w-[380px] bg-black border-2 border-white rounded-2xl" type="email" name="" id="" />
+            <button className="h-[59px] w-[80px] rounded-2xl -ml-[83px] font-semibold hover:bg-black hover:text-white hover:border-white hover:border-2 transition-all duration-300 bg-white">Send</button>
+          </div>
         </div>
       </div>
     </div>
