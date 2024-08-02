a6d18cbaf9eb24f6be03fbf215c51f88e2472a34
Akash M Nandan <akash003nandan@gmail.com>
2024-08-02 14:42:09 +0530

    subscibe text added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,14 @@
 const Footer = () => {
   return (
     <div className="h-[500px] w-full px-24 pt-16">
-      <div className="h-[400px] w-full bg-black rounded-3xl">
-
+      <div className="h-[400px] w-full bg-black rounded-3xl px-20 py-16">
+        <div className="">
+          <div className="flex flex-col">
+            <p className="h-[80px] w-[250px] border-white border-2 bg-green-700 text-4xl text-white rounded-full px-8 py-5">Subscribe</p>
+            <p className="h-[80px] w-[345px] border-white border-2 text-4xl text-white rounded-full px-9 py-5">to our updates</p>
+          </div>
+          
+        </div>
       </div>
     </div>
   )
