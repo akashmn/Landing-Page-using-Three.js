1406b1b43c19a5178590143ffee927388ac9f9d1
Akash M Nandan <akash003nandan@gmail.com>
2024-08-03 16:07:55 +0530

    about us full responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,9 +3,11 @@
 const About = () => {
   return (
     <div className="py-3 flex flex-row">
-        <div className="h-screen max-sm:h-[2000px] w-full bg-green-600 px-[250px] max-sm:px-[5px] py-28 max-sm:py-8 flex flex-row max-sm:flex-col items-center justify-between">
-            <div className="">
+        <div className="h-screen max-sm:h-[1300px] w-full bg-green-600 px-[250px] max-sm:px-[5px] py-28 max-sm:py-8 flex flex-row max-sm:flex-col-reverse items-center justify-between">
+            <div>
                 <div 
+                    data-aos='zoom-in'
+                    data-aos-duration='1000'
                     className="rotate-[-12deg] h-[260px] w-[230px] max-sm:h-[180px] max-sm:w-[190px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 max-sm:gap-3 hover:bg-sky-500 transition-all duration-300 -z-20">
                     <div 
                          className="h-[70px] max-sm:h-[40px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
@@ -22,6 +24,8 @@ const About = () => {
                 </div>
 
                 <div 
+                    data-aos='zoom-in'
+                    data-aos-duration='1000'
                     className="rotate-[-12deg] h-[260px] w-[230px] max-sm:h-[180px] max-sm:w-[190px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 max-sm:gap-3 hover:bg-sky-500 transition-all duration-300 -z-20">
                     <div 
                          className="h-[70px] max-sm:h-[40px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
@@ -38,6 +42,8 @@ const About = () => {
                 </div>
 
                 <div 
+                    data-aos='zoom-in'
+                    data-aos-duration='1000'
                     className="rotate-[-12deg] h-[260px] w-[230px] max-sm:h-[180px] max-sm:w-[190px] border-2 p-3 bg-white border-black rounded-2xl flex flex-col items-center justify-start gap-2 max-sm:gap-3 hover:bg-sky-500 transition-all duration-300 -z-20">
                     <div 
                          className="h-[70px] max-sm:h-[40px] w-full  bg-white border-black border-2 rounded-2xl flem items-center justify-center">
@@ -53,10 +59,10 @@ const About = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center gap-10 ml-52 max-sm:ml-7 max-sm:p-3">
-                <p className="text-5xl font-bold tracking-wide">Presenting Ourselves!</p>
-                <p className="text-lg max-sm:text-lg tracking-wider">Our team of passionate designers, developers, and marketing specialists collaborates to bring your vision to life. We create beautiful, user-friendly websites and mobile apps, craft compelling design elements that elevate your brand, and implement data-driven digital marketing strategies to reach your target audience.</p>
-                <p className="text-lg max-sm:text-lg tracking-wider">At Qmark Technolabs, we offer a spectrum of services designed to meet the diverse needs of businesses today.</p>
+            <div className="flex flex-col max-sm:flex-col-reverse items-center justify-center gap-10 ml-52 max-sm:ml-7 max-sm:p-3">
+                <p data-aos='fade-left' data-aos-duration='1000' className="text-5xl font-bold tracking-wide">Presenting Ourselves!</p>
+                <p data-aos='fade-left' data-aos-duration='1000' className=" text-lg max-sm:text-lg tracking-wider">Our team of passionate designers, developers, and marketing specialists collaborates to bring your vision to life. We create beautiful, user-friendly websites and mobile apps, craft compelling design elements that elevate your brand, and implement data-driven digital marketing strategies to reach your target audience.</p>
+                <p data-aos='fade-left' data-aos-duration='1000' className=" text-lg max-sm:text-lg tracking-wider">At Qmark Technolabs, we offer a spectrum of services designed to meet the diverse needs of businesses today.</p>
             </div>
         </div>
     </div>
