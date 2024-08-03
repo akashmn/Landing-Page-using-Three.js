9aa9ec1f5195a462f2e86b508e2df43c03cd60f1
Akash M Nandan <akash003nandan@gmail.com>
2024-08-03 17:24:01 +0530

    navbar cconatiner responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,45 +1,51 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown, faCaretUp, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   const [popUp, setPopUp] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [activeLink, setActiveLink] = useState('Home');
 
   const togglePopup = () => {
     setPopUp(!popUp);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   const handleLinkClick = (link) => {
     setActiveLink(link);
+    setMenuOpen(false); // Close the menu when a link is clicked
   };
 
   return (
-    <div className="px-6 pt-4">
-      <div className="h-[75px] w-full rounded-full border-2 border-black flex flex-row items-center justify-between px-16">
+    <div className="px-6 pt-4 relative">
+      <div className="h-[75px] w-full rounded-full border-2 border-black flex flex-row items-center justify-between px-6 lg:px-16">
         <p>Qmark Technolabs</p>
-        <div className="flex flex-row gap-10">
+        <div className="hidden lg:flex flex-row gap-10">
           <a
-              href="#"
-              className={`${activeLink === 'Home' ? 'font-bold underline' : ''}`}
-              onClick={() => handleLinkClick('Home')}
-            >
-              Home
-            </a>
-            <a
-              href="#"
-              className={`${activeLink === 'Works' ? 'font-bold underline' : ''}`}
-              onClick={() => handleLinkClick('Works')}
-            >
-              Works
-            </a>
-            <a
-              href="#"
-              className={`${activeLink === 'Team' ? 'font-bold underline' : ''}`}
-              onClick={() => handleLinkClick('Team')}
-            >
-              Team
-            </a>
+            href="#"
+            className={`${activeLink === 'Home' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('Home')}
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className={`${activeLink === 'Works' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('Works')}
+          >
+            Works
+          </a>
+          <a
+            href="#"
+            className={`${activeLink === 'Team' ? 'font-bold underline' : ''}`}
+            onClick={() => handleLinkClick('Team')}
+          >
+            Team
+          </a>
           <button onClick={togglePopup}>
             <FontAwesomeIcon
               icon={popUp ? faCaretUp : faCaretDown}
@@ -50,12 +56,15 @@ const Navbar = () => {
             />
           </button>
         </div>
-        <button className='transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black'>
+        <button className="hidden lg:block transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black">
           Get in Touch
+        </button>
+        <button className="lg:hidden" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} size='xl' />
         </button>
       </div>
       {popUp && (
-        <div data-aos="zoom-in-down" className='ml-[750px] -mt-4 p-6 z-20 bg-black h-[200px] w-[250px] absolute rounded-xl transition duration-700 ease-in-out opacity-95 flex flex-col gap-2'>
+        <div data-aos="zoom-in-down" className="ml-[750px] -mt-4 p-6 z-20 bg-black h-[200px] w-[250px] absolute rounded-xl transition duration-700 ease-in-out opacity-95 flex flex-col gap-2">
           <a
             href="#"
             className={`text-white p-3 hover:underline ${activeLink === 'Careers' ? 'font-bold underline' : ''}`}
@@ -79,6 +88,61 @@ const Navbar = () => {
           </a>
         </div>
       )}
+      <div
+        className={`lg:hidden absolute top-16 left-0 right-0 z-20 transition-all duration-500 ease-in-out overflow-hidden ${
+          menuOpen ? 'max-h-[400px] opacity-100' : 'max-h-0 opacity-0'
+        } bg-white border-black border-2 rounded-b-3xl`}
+      >
+        {menuOpen && (
+          <div className="flex flex-col gap-4 p-6">
+            <a
+              href="#"
+              className={`text-black p-3 border-b border-black ${activeLink === 'Home' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Home')}
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className={`text-black p-3 border-b border-black ${activeLink === 'Works' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Works')}
+            >
+              Works
+            </a>
+            <a
+              href="#"
+              className={`text-black p-3 border-b border-black ${activeLink === 'Team' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Team')}
+            >
+              Team
+            </a>
+            <a
+              href="#"
+              className={`text-black p-3 border-b border-black ${activeLink === 'Careers' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Careers')}
+            >
+              Careers
+            </a>
+            <a
+              href="#"
+              className={`text-black p-3 border-b border-black ${activeLink === 'Services' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('Services')}
+            >
+              Services
+            </a>
+            <a
+              href="#"
+              className={`text-black p-3 ${activeLink === 'More' ? 'font-bold underline' : ''}`}
+              onClick={() => handleLinkClick('More')}
+            >
+              More
+            </a>
+            <button className="transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 mt-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black">
+              Get in Touch
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
