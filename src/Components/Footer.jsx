b8c132edd44c40c9d2be7dc6c8c8b8ba911e4db2
Akash M Nandan <akash003nandan@gmail.com>
2024-08-03 13:11:13 +0530

    footer subscribe container  responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col">
           <div className="flex flex-col">
             <p className="h-[80px] w-[250px] max-sm:h-[45px] max-sm:w-[150px] border-white border-2 bg-green-700 text-4xl max-sm:text-2xl text-white rounded-full px-8 py-5 max-sm:px-3 max-sm:py-1">Subscribe</p>
-            <p className="h-[80px] w-[345px] border-white border-2 text-4xl text-white rounded-full px-9 py-5">to our updates</p>
+            <p className="h-[80px] w-[345px] max-sm:h-[45px] max-sm:w-[220px] border-white border-2 text-4xl max-sm:text-2xl text-white rounded-full px-9 py-5 max-sm:px-4 max-sm:py-1">to our updates</p>
           </div>
           <div className="mt-10">
             <input className="h-[65px] w-[380px] bg-black border-2 border-white rounded-2xl text-white placeholder:text-white placeholder:opacity-60 pl-8" type="email" placeholder="Enter your email" name="" id="" />
