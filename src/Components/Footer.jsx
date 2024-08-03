b641cd47418765b6828f30587a6ee0a7d70d313e
Akash M Nandan <akash003nandan@gmail.com>
2024-08-03 13:41:12 +0530

    footer 1sst container responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,15 +4,15 @@ import { faFacebookF, faGithub, faInstagram, faXTwitter } from '@fortawesome/fre
 const Footer = () => {
   return (
     <div className="h-[500px] max-sm:h-screen w-full px-24 max-sm:px-3 pt-16 max-sm:pt-3 max-sm:pb-3">
-      <div className="w-full bg-black rounded-3xl px-20 py-14 flex flex-row max-sm:flex-col gap-44">
+      <div className="w-full bg-black rounded-3xl px-20 py-14 max-sm:px-10 max-sm:py-7 flex flex-row max-sm:flex-col gap-44">
         <div className="flex flex-col">
           <div className="flex flex-col">
             <p className="h-[80px] w-[250px] max-sm:h-[45px] max-sm:w-[150px] border-white border-2 bg-green-700 text-4xl max-sm:text-2xl text-white rounded-full px-8 py-5 max-sm:px-3 max-sm:py-1">Subscribe</p>
             <p className="h-[80px] w-[345px] max-sm:h-[45px] max-sm:w-[220px] border-white border-2 text-4xl max-sm:text-2xl text-white rounded-full px-9 py-5 max-sm:px-4 max-sm:py-1">to our updates</p>
           </div>
-          <div className="mt-10">
-            <input className="h-[65px] w-[380px] bg-black border-2 border-white rounded-2xl text-white placeholder:text-white placeholder:opacity-60 pl-8" type="email" placeholder="Enter your email" name="" id="" />
-            <button className="h-[59px] w-[90px] rounded-2xl -ml-[93px] font-semibold hover:bg-black hover:text-white hover:border-white hover:border-2 transition-all duration-300 bg-white">Send</button>
+          <div className="mt-10 max-sm:mt-3">
+            <input className="h-[65px] w-[380px] max-sm:h-[35px] max-sm:w-[255px] bg-black border-2 border-white rounded-2xl text-white max-sm:text-sm placeholder:text-white placeholder:opacity-60 pl-8 max-sm:pl-4" type="email" placeholder="Enter your email" name="" id="" />
+            <button className="h-[59px] w-[90px] max-sm:h-[30px] max-sm:w-[45px] rounded-2xl -ml-[93px] max-sm:-ml-[48px] font-semibold hover:bg-black hover:text-white max-sm:text-sm hover:border-white hover:border-2 transition-all duration-300 bg-white">Send</button>
           </div>
         </div>
         <div className='flex flex-row max-sm:flex-col'>
