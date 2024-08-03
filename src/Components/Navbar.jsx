2abdf236acab78aea41cf5c36d5e907c87d94afa
Akash M Nandan <akash003nandan@gmail.com>
2024-08-03 17:34:45 +0530

    navbar menu container responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -89,55 +89,55 @@ const Navbar = () => {
         </div>
       )}
       <div
-        className={`lg:hidden absolute top-16 left-0 right-0 z-20 transition-all duration-500 ease-in-out overflow-hidden ${
-          menuOpen ? 'max-h-[400px] opacity-100' : 'max-h-0 opacity-0'
-        } bg-white border-black border-2 rounded-b-3xl`}
+        className={`mx-5 mt-8 lg:hidden absolute top-16 left-0 right-0 z-20 transition-all duration-500 ease-in-out overflow-hidden ${
+          menuOpen ? 'max-h-[420px] opacity-100' : 'max-h-0 opacity-0'
+        } bg-white border-black border-2 rounded-3xl`}
       >
         {menuOpen && (
-          <div className="flex flex-col gap-4 p-6">
+          <div className="flex flex-col gap-2 bg-black">
             <a
               href="#"
-              className={`text-black p-3 border-b border-black ${activeLink === 'Home' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 border-b border-black ${activeLink === 'Home' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('Home')}
             >
               Home
             </a>
             <a
               href="#"
-              className={`text-black p-3 border-b border-black ${activeLink === 'Works' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 border-b border-black ${activeLink === 'Works' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('Works')}
             >
               Works
             </a>
             <a
               href="#"
-              className={`text-black p-3 border-b border-black ${activeLink === 'Team' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 border-b border-black ${activeLink === 'Team' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('Team')}
             >
               Team
             </a>
             <a
               href="#"
-              className={`text-black p-3 border-b border-black ${activeLink === 'Careers' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 border-b border-black ${activeLink === 'Careers' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('Careers')}
             >
               Careers
             </a>
             <a
               href="#"
-              className={`text-black p-3 border-b border-black ${activeLink === 'Services' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 border-b border-black ${activeLink === 'Services' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('Services')}
             >
               Services
             </a>
             <a
               href="#"
-              className={`text-black p-3 ${activeLink === 'More' ? 'font-bold underline' : ''}`}
+              className={`text-white p-3 ${activeLink === 'More' ? 'font-bold underline' : ''}`}
               onClick={() => handleLinkClick('More')}
             >
               More
             </a>
-            <button className="transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 mt-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black">
+            <button className="max-sm:hidden transition-all duration-300 bg-black text-white h-[40px] w-[150px] px-4 mt-4 rounded-full hover:bg-white hover:text-black hover:border-2 hover:border-black">
               Get in Touch
             </button>
           </div>
